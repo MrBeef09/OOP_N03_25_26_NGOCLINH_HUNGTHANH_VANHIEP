6599004c6aeed13ff3bac99bd6a64977310618df
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-08-22 17:15:31 +0000

    update

---
+++ ourproject/src/Model/Facilities.java
@@ -0,0 +1,11 @@
+public class Facilities extends Universities{
+    public int chisoGiaoVien;
+    public double tyleGiaoVienvsSinhVien;
+    public int soLuongPhongHoc;
+    public int soLuongPhongLab;
+    public String chatLuongPhongHoc;
+    public String chatLuongPhongLab;
+    public String heThongQuanLy;
+    public int chiSoTrangBietBiTheThao;
+
+}
