afb7fcaecfb52f76a07c0f6c901ad82eee53bd03
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-08-22 17:15:31 +0000

    update

---
+++ ourproject/src/Model/Major.java
@@ -0,0 +1,24 @@
+public class Major extends Universities {
+    public int maNghanh;
+    public string tenNghanh;
+    private int diemChuan;
+    private int diemThi;
+
+
+    public int getdiemChuan(){
+        return diemChuan;
+    }
+    public void setdiemChuan(int Diemchuann){
+        diemChuan = Diemchuann;
+    }
+   
+    public int getdiemthi(){
+        return diemThi;
+    }
+    public void setdiemThi(int Diemthi){
+        diemThi = Diemthi;
+    }
+    void kiemTra(){
+
+    }
+}
