5b2133e63f0576e29daf02bc72cde0cc1eb054f3
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-08-25 16:03:48 +0000

    update diem

--- ourproject/src/App.java
+++ ourproject/src/App.java
@@ -1,6 +1,12 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Nhom ban  Linh Thanh Hiep!");
         testUniversities.test();
+
+        Major sv = new Major();
+        sv.setDiemChuan(24);
+        sv.setDiemThi(26);
+        sv.setTenNganh("Công nghệ thông tin"); 
+        sv.kiemTra();
     }
 }
--- ourproject/src/Model/Major.java
+++ ourproject/src/Model/Major.java
@@ -1,24 +1,43 @@
 public class Major extends Universities {
-    public int maNghanh;
-    public string tenNghanh;
+    private int maNganh;
+    private String tenNganh;   
     private int diemChuan;
     private int diemThi;
 
-
-    public int getdiemChuan(){
+    // Getter & Setter
+    public int getDiemChuan() {
         return diemChuan;
     }
-    public void setdiemChuan(int Diemchuann){
-        diemChuan = Diemchuann;
+    public void setDiemChuan(int diemChuan) {
+        this.diemChuan = diemChuan;
     }
-   
-    public int getdiemthi(){
+
+    public int getDiemThi() {
         return diemThi;
     }
-    public void setdiemThi(int Diemthi){
-        diemThi = Diemthi;
+    public void setDiemThi(int diemThi) {
+        this.diemThi = diemThi;
     }
-    void kiemTra(){
 
+    public int getMaNganh() {
+        return maNganh;
+    }
+    public void setMaNganh(int maNganh) {
+        this.maNganh = maNganh;
+    }
+
+    public String getTenNganh() {
+        return tenNganh;
+    }
+    public void setTenNganh(String tenNganh) {
+        this.tenNganh = tenNganh;
+    }
+
+    void kiemTra() {
+        if (diemThi >= diemChuan) {
+            System.out.println("Đậu ngành: " + tenNganh);
+        } else {
+            System.out.println("Trượt ngành: " + tenNganh);
+        }
     }
 }
