0f5d7180035b553243df4f5738ad2a8ce0df989a
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-09-04 07:23:31 +0000

    nguyenhungthanh

--- ourproject/src/Test/testUniversities.java
+++
@@ -1,8 +0,0 @@
-public class testUniversities {
-
-    public static void test(){
-        Universities uni = new Universities();
-        uni.print("PSA", "Dai hoc Phenikaa", "Duong noi Hanoi");
-    }
-    
-}
