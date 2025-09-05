7719bbcd266c77b7b8b8c5cc551dad0c8bd75226
maingoclinh2005 <156213585+maingoclinh2005@users.noreply.github.com>
2025-09-05 10:35:39 +0000

    baitapphan2

---
+++ ourproject/review/exercise1pkg/MyClass.java
@@ -0,0 +1,7 @@
+package review.exercise1pkg;
+
+public class MyClass {
+    public void sayHello() {
+        System.out.println("Hello from MyClass in package review.exercise1pkg!");
+    }
+}
---
+++ ourproject/review/exercise3pkg/debugoff/Debug.java
@@ -0,0 +1,7 @@
+package review.exercise3pkg.debugoff;
+
+public class Debug {
+    public static void debug(String msg) {
+        // Không làm gì cả
+    }
+}
