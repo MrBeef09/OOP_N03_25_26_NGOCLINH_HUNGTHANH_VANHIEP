012fdf87aa05f617f4b633a8fe5d810ea2f5e871
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-09-05 11:01:37 +0000

    thanh

---
+++ ourproject/src/Model/User/User.java
@@ -0,0 +1,30 @@
+//package User;
+public class User {
+    private String Hoten;
+    private int Ngaysinh;
+    private String Gioitinh;
+
+    //getter setter
+    public String getHoten(){
+        return Hoten;
+    }
+    public int getNgaysinh(){
+        return Ngaysinh;
+    }
+    public String getGioitinh(){
+        return Gioitinh;
+    }
+
+    public void setNguoi(String Hoten , int Ngaysinh , String Gioitinh){
+        this.Hoten = Hoten;
+        this.Ngaysinh = Ngaysinh;
+        this.Gioitinh = Gioitinh;
+    }
+
+    //method
+    public void xemThongtin(){
+        System.out.println("Ho va ten:" + Hoten);
+        System.out.println("Ngay sinh:" + Ngaysinh);
+        System.out.println("Gioi tinh:" + Gioitinh); 
+    }
+}
