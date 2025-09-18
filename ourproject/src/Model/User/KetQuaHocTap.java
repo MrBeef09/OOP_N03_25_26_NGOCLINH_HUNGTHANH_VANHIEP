67baea23d2fc6a2d1d0abfdbf9daf4a3042e7c20
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-09-18 15:19:02 +0000

    thanh sua loi

---
+++ ourproject/src/Model/User/KetQuaHocTap.java
@@ -0,0 +1,29 @@
+public class KetQuaHocTap {
+    private float DiemThi;
+    private String HocLuc;
+    private String ToHopMon;
+
+    //getter setter
+    public float GetDiemThi(){
+        return DiemThi;
+    }
+    public String GetHocLuc(){
+        return HocLuc;
+    }
+    public String GetToHopMon(){
+        return ToHopMon;
+    }
+    public void SetDiemThi(float DiemThi){
+        this.DiemThi = DiemThi;
+    }
+    public void SetHocLuc(String HocLuc){
+        this.HocLuc = HocLuc;
+    }
+    public void SetToHopMon(String ToHopMon){
+        this.ToHopMon = ToHopMon;
+    }
+    
+    //method 
+    public void Capnhatdiemthi(){}
+    public void Tinhhocluc(){}
+}
---
+++ ourproject/src/Model/User/SoThichNganhHoc.java
@@ -0,0 +1,31 @@
+public class SoThichNganhHoc {
+    private String NganhQuanTam;
+    private String SoThich;
+    private String TaiNang;
+
+    // getter setter
+    public String GetNganhQuanTam(){
+        return NganhQuanTam;
+    }
+    
+    public String GetSoThich(){
+        return SoThich;
+    }
+    
+    public String GetTaiNang(){
+        return TaiNang;
+    }
+    public void SetNganhQuanTam(String NganhQuanTam) {
+        this.NganhQuanTam = NganhQuanTam;
+    }
+    public void SetSoThich(String SoThich) {
+        this.SoThich = SoThich;
+    }
+    public void SetTaiNang(String TaiNang) {
+        this.TaiNang = TaiNang;
+    }
+
+    // method
+    public void Chonnganhhoc(){}
+    public void Xemsothich(){}
+}
