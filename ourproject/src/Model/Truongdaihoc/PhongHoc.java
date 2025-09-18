fcc8661a5e26ec3eed6f3b1164e68c204137505b
maingoclinh2005 <156213585+maingoclinh2005@users.noreply.github.com>
2025-09-18 15:50:26 +0000

    linh

---
+++ ourproject/src/Model/Truongdaihoc/PhongHoc.java
@@ -0,0 +1,22 @@
+public abstract class PhongHoc{
+    protected String maPhong;
+    protected int sucChua;
+    protected int soMayChieu;
+    protected int soDieuHoa;
+    protected int soQuat;
+    
+    public PhongHoc(String maPhong,int sucChua){
+        this.maPhong = maPhong;
+        this.sucChua = sucChua;
+    }
+    public String getMaPhong(){}
+
+
+
+
+
+
+
+
+}
+
