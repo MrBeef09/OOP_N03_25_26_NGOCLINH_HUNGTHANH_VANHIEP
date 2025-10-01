dbf0518d54145138aba0bdacd61ce416469899bb
Hiệp <hiepvbhp2005@gmail.com>
2025-10-01 16:04:31 +0000

    hiep

--- ourproject/src/Model/Truongdaihoc/TruongDaiHoc.java
+++ ourproject/src/Model/Truongdaihoc/TruongDaiHoc.java
@@ -74,31 +74,30 @@ public class TruongDaiHoc {
     }
 }
 
-// Không public để ở chung file được
-class DoiNguGiaoVien {
-    private int soLuongGV;
-    private int soLuongTS; // số tiến sĩ
-    private int soLuongThS; // số thạc sĩ
-    private int kinhNghiemTB; // số năm kinh nghiệm trung bình
+    class DoiNguGiaoVien {
+        private int soLuongGV;
+        private int soLuongTS; // số tiến sĩ
+        private int soLuongThS; // số thạc sĩ
+        private int kinhNghiemTB; // số năm kinh nghiệm trung bình
 
-    public DoiNguGiaoVien(int soLuongGV, int soLuongTS, int soLuongThS, int kinhNghiemTB) {
-        this.soLuongGV = soLuongGV;
-        this.soLuongTS = soLuongTS;
-        this.soLuongThS = soLuongThS;
-        this.kinhNghiemTB = kinhNghiemTB;
-    }
+        public DoiNguGiaoVien(int soLuongGV, int soLuongTS, int soLuongThS, int kinhNghiemTB) {
+            this.soLuongGV = soLuongGV;
+            this.soLuongTS = soLuongTS;
+            this.soLuongThS = soLuongThS;
+            this.kinhNghiemTB = kinhNghiemTB;
+        }
 
-    public void xemThongTin() {
-        System.out.println("Giáo viên: " + soLuongGV +
-                        " | TS: " + soLuongTS +
-                        " | ThS: " + soLuongThS +
-                        " | Kinh nghiệm TB: " + kinhNghiemTB + " năm");
-    }
+        public void xemThongTin() {
+            System.out.println("Giáo viên: " + soLuongGV +
+                            " | TS: " + soLuongTS +
+                            " | ThS: " + soLuongThS +
+                            " | Kinh nghiệm TB: " + kinhNghiemTB + " năm");
+        }
 
-    public int danhGia() {
-        // ví dụ: 1 điểm cho mỗi 10 GV
-        // 2 điểm cho mỗi TS, 1 điểm cho mỗi ThS
-        // + kinh nghiệm trung bình chia 5
-        return (soLuongGV / 10) + (soLuongTS * 2) + (soLuongThS) + (kinhNghiemTB / 5);
-    }
+        public int danhGia() {
+            // ví dụ: 1 điểm cho mỗi 10 GV
+            // 2 điểm cho mỗi TS, 1 điểm cho mỗi ThS
+            // + kinh nghiệm trung bình chia 5
+            return (soLuongGV / 10) + (soLuongTS * 2) + (soLuongThS) + (kinhNghiemTB / 5);
+        }
 }
