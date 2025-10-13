5cf48042732137b66c4b8c582e06a94470fcdd4e
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-10-13 16:12:31 +0000

    .

--- gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Controller/CoSoVatChatController.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Controller/CoSoVatChatController.java
@@ -1,20 +1,6 @@
-package com.example.servingwebcontent.Controller;
-
-import com.example.servingwebcontent.service.CoSoVatChatService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class CoSoVatChatController {
-
-    @Autowired
-    private CoSoVatChatService coSoVatChatService;
+cte CoSoVatChatService coSoVatChatService;
 
     @GetMapping("/cosovatchat")
     public String hienThiThongTinCSVC(Model model) {
         model.addAttribute("coSoVatChat", coSoVatChatService.layDanhSachCSVC());
-        return "cosovatchat"; // file cosovatchat.html trong templates
-    }
-}
+        return
