eace82ab955429846dc44205d2435070ea479793
Nguyen Hung Thanh <160729125+MrBeef09@users.noreply.github.com>
2025-10-18 03:48:37 +0000

    thanh2thanh

---
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Repository/TruongDaiHocRepository.java
@@ -0,0 +1,11 @@
+package com.example.servingwebcontent.Repository;
+
+import com.example.servingwebcontent.Model.Truongdaihoc.TruongDaiHoc;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Interface này tự động kế thừa các phương thức CRUD cơ bản (findAll, save, findById,...)
+// Long là kiểu dữ liệu của khóa chính (id) trong lớp TruongDaiHoc
+@Repository
+public interface TruongDaiHocRepository extends JpaRepository<TruongDaiHoc, Long> {
+}
