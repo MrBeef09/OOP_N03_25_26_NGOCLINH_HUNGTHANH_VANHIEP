f684aee34c0939f374bbb0ef2af0a1146925732c
HungThanh-web <23010688@st.phenikaa-uni.edu.vn>
2025-11-06 11:25:51 +0000

    hiep

--- gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Controller/AdminDashboardController.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Controller/AdminDashboardController.java
@@ -1,5 +1,6 @@
 package com.example.servingwebcontent.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
